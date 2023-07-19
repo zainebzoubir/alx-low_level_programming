7d0ea439884760a90cd73101787a95e4e48b309a
zainebzoubir <me@example.com>
2023-07-18 18:09:06 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	} else if (n == 0)
 	}
@@ -19,7 +19,7 @@ int print_sign(int n)
 		return (0);
 	} else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 	}
 		return (-1);
 }
